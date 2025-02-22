a7e91a0231ec818c3ff5f81ad00a3c45bb1b1a91
naman saxena <namansaxena661@gmail.com>
2025-02-22 21:45:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import News from './components/News';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import News from "./components/News";
 
 function App(props) {
   return (
     <>
-
-      <div className="main" style={{ backgroundColor: "whiteSmoke", fontFamily: 'Poppins, sansSerif', marginTop: "-45px" }}>
+      <div
+        className="main"
+        style={{
+          backgroundColor: "whiteSmoke",
+          fontFamily: "Poppins, sansSerif",
+          marginTop: "-45px",
+        }}
+      >
         <Navbar />
-        <News title={props.title} description={props.description} imageUrl={props.urlToImage} newsUrl={props.url} author={props.author} date={props.publishedAt} />
+        <News
+          title={props.title}
+          description={props.description}
+          imageUrl={props.urlToImage}
+          newsUrl={props.url}
+          author={props.author}
+          date={props.publishedAt}
+        />
         <Footer />
       </div>
     </>
-
   );
 }
 
